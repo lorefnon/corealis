5b2a2446ddcf05e7c72aab3a1ae7e10afc399d68
Lorefnon <lorefnon@gmail.com>
2016-02-07 10:57:48 +0530

    rails g devise:install ; rails g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
