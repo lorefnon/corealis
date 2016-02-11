34873938c44f9ab7ef5912731fea16f19c58e759
Lorefnon <lorefnon@gmail.com>
2016-02-11 10:15:40 +0530

    Expose user in admin

--- app/admin/invitation.rb
+++ app/admin/invitation.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Invitation do
 
-  menu priority: 4
+  menu priority: 5
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
