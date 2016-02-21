68212a3f640ed852aa808931c6428a92c01f7177
Lorefnon <lorefnon@gmail.com>
2016-02-21 13:24:02 +0530

    Update spec stubs

--- spec/models/online_resource.rb
+++ spec/models/online_resource.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe OnlineAccountProvider, type: :model do
+RSpec.describe OnlineResource, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
