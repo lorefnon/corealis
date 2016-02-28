0ac87b2b58b33cf0024fa1c6d12e8294d03f5782
Lorefnon <lorefnon@gmail.com>
2016-02-28 22:37:46 +0530

    Remove scripts for downloading mailhog

--- scripts/mailhog_installer.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Download binary from github
-echo ">>> Downloading Mailhog"
-wget -O ~/mailhog https://github.com/mailhog/MailHog/releases/download/v0.1.8/MailHog_darwin_amd64
-echo ">>> Downloaded Mailhog"
-
-# Make it executable
-chmod +x ~/mailhog
