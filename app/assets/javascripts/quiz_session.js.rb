94cbcf7301a0ccf718a2b0056d90a9a2e95d7f6a
Lorefnon <lorefnon@gmail.com>
2016-03-02 02:25:07 +0530

    wip

--- app/assets/javascripts/quiz_session.js.rb
+++ app/assets/javascripts/quiz_session.js.rb
@@ -1,1 +1,11 @@
 require_relative './application'
+require 'clearwater'
+
+router = Clearwater::Router.new do
+  route 'question'
+end
+
+Clearwater::Application.new(
+  component: QuizSession::Components::Root.new,
+  router: router
+)
