bdb76bd21bdb9632227b0790704fa20fbb6d07bb
Lorefnon <lorefnon@gmail.com>
2016-03-06 12:24:37 +0530

    Add support for conditionally redirecting to home url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+
+  before_action :redirect_to_home_url, only: :index
+
+  private
+
+  def redirect_to_home_url
+    if home_url = Setting['organization.home_url']
+      redirect_to home_url
+    end
+  end
+
 end
