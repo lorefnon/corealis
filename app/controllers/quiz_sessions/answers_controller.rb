21f2e614418bb6c9b397954f829f7c8a0be88c18
Lorefnon <lorefnon@gmail.com>
2016-03-19 18:50:47 +0530

    Fix issues

--- app/controllers/quiz_sessions/answers_controller.rb
+++ app/controllers/quiz_sessions/answers_controller.rb
@@ -1,8 +1,14 @@
 module QuizSessions
   class AnswersController < ApplicationController
 
+    before_action :load_quiz_session
+
     def create
-      @answer = @quiz_session.answers.create!(answer_params)
+      # TODO Handle applicant here
+      @answer = @quiz_session.answers.create!(answer_params.merge(answerer: current_admin_user))
+      if @current_question = @quiz_session.next_question
+        @current_answer = @quiz_session.answers.new(question: @current_question)
+      end
     end
 
     private
