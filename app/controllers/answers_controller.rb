45b0fa40e204872b1aea631ab5146e2f1dd90212
Lorefnon <lorefnon@gmail.com>
2016-03-28 01:40:14 +0530

    Enhancements

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,9 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.create!(answer_params)
-    if @current_question = @quiz_session.next_question
+    @quiz_session = @answer.quiz_session
+    if next_question = @quiz_session.next_question
+      @current_question = next_question.decorate
       @current_answer = @quiz_session.answers.new(question: @current_question)
     end
   end
