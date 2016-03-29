29345e0f69b7a52a9f32bafda94287ac11a1ad1a
Lorefnon <lorefnon@gmail.com>
2016-03-29 09:25:22 +0530

    Cleanup method arguments

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -4,10 +4,10 @@ class ApplicationAuthorizer < Authority::Authorizer
   # Any class method from Authority::Authorizer that isn't overridden
   # will call its authorizer's default method.
   #
-  # @param [Symbol] adjective; example: `:creatable`
-  # @param [Object] user - whatever represents the current user in your app
+  # @param [Symbol] _adjective; example: `:creatable`
+  # @param [Object] _user - whatever represents the current user in your app
   # @return [Boolean]
-  def self.default(adjective, user)
+  def self.default(_adjective, _user)
     # 'Whitelist' strategy for security: anything not explicitly allowed is
     # considered forbidden.
     false
