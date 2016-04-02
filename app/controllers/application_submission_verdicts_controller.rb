288c679dc29d61cf0d2a33255788c6cb065df859
Lorefnon <lorefnon@gmail.com>
2016-04-02 15:52:18 +0530

    Visual enhancements

--- app/controllers/application_submission_verdicts_controller.rb
+++ app/controllers/application_submission_verdicts_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationSubmissionVerdictsController < ApplicationController
+
+  layout 'single_panel'
+
   def show
     @verdict = params[:verdict]
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def bs_alert_class(name)
+    {
+      'error' => 'alert-danger',
+      'success' => 'alert-success'
+    }[name] || 'alert-info'
+  end
+
 end
