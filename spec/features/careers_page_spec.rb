9673fea0e8d5f11466985e02fe4446187f007984
Lorefnon <lorefnon@gmail.com>
2016-04-02 18:53:06 +0530

    Improve specs

--- spec/features/careers_page_spec.rb
+++ spec/features/careers_page_spec.rb
@@ -2,37 +2,26 @@ require 'rails_helper'
 require 'support/matchers/have_node_with_content'
 require 'support/features/opening_helpers'
 
-feature 'Home Page' do
+feature 'Careers Page' do
 
   include Features::OpeningHelpers
 
-  before(:all) do
-    Setting['organization.name'] = @organization_name = 'Model Corporation'
-    Setting['organization.description'] = @organization_description = 'Lorem ipsum dolor sit amet'
-  end
-
-  scenario 'home page is visited' do
-    visit root_path
-    expect(page).to have_content @organization_name
-    expect(page).to have_content @organization_description
-  end
-
   scenario 'Openings are available' do
     openings = create_openings_of_all_variants
-    visit root_path
+    visit openings_path
     openings.each do |(current_status, showcased_status), opening|
-      if current_status == :current && showcased_status == :showcased
+      if current_status == :current
         expect(page).to have_link opening.title, href: opening_path(opening)
       else
         expect(page).to_not have_link opening.title, href: opening_path(opening)
       end
     end
-    expect(page).to have_link 'View All', href: openings_path
+    expect(page).to_not have_link 'View All', href: openings_path
   end
 
   scenario 'No openings are available' do
     Opening.destroy_all
-    visit root_path
+    visit openings_path
     expect(find('.zilch-container')).to have_content 'There are no open positions right now'
   end
 
--- spec/support/features/opening_helpers.rb
+++ spec/support/features/opening_helpers.rb
@@ -5,10 +5,10 @@ module Features
 
     def create_openings_of_all_variants
       {
-        [:current, :showcased] => create(:opening, :showcased),
-        [:current, :non_showcased] => create(:opening),
-        [:expired, :showcased] => create(:opening, :showcased, :expired),
-        [:expired, :non_showcased] => create(:opening, :expired)
+        [:current, :showcased]      => FactoryGirl.create(:opening, :showcased),
+        [:current, :non_showcased]  => FactoryGirl.create(:opening),
+        [:expired, :showcased]      => FactoryGirl.create(:opening, :showcased, :expired),
+        [:expired, :non_showcased]  => FactoryGirl.create(:opening, :expired)
       }
     end
 
