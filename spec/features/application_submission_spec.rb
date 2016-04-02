14a9e4fc635be978ef9b557f8c4978895184cefc
Lorefnon <lorefnon@gmail.com>
2016-04-02 23:09:34 +0530

    Improve specs

--- spec/features/application_submission_spec.rb
+++ spec/features/application_submission_spec.rb
@@ -2,26 +2,35 @@ require 'rails_helper'
 require 'support/features/opening_helpers'
 require 'support/features/application_submission_helpers'
 
-feature 'application submission for opening' do
+feature 'Application Submission' do
 
   include Features::OpeningHelpers
   include Features::ApplicationSubmissionHelpers
 
-  scenario 'Applying for opening from home page' do
-    opening = create(:opening, :showcased)
-    visit_opening_from_home_page opening
-    applicant = build(:applicant)
-    application_submission = build(
-      :application_submission,
-      applicant: applicant,
-      opening: opening
-    )
-    fill_applicant_details applicant
-    fill_submission_details application_submission
-    click_on 'Create Application submission'
-    expect(current_path).to eq application_submission_verdict_path(verdict: 'success')
-    expect(page).to have_content "Congratulations !"
-    expect(page).to have_content "Your application was successfully submitted"
+  context 'User has not already applied for the position' do
+    scenario 'Applying from Home Page' do
+      opening = create(:opening, :showcased)
+      visit_opening_from_home_page opening
+      application_submission = build(:application_submission, opening: opening)
+      applicant = application_submission.applicant
+      fill_applicant_details applicant
+      fill_submission_details application_submission
+      click_on 'Create Application submission'
+      expect_submission_success
+    end
+  end
+
+  context 'User has already applied for position' do
+    scenario 'Applying from Home Page' do
+      opening = create(:opening, :showcased)
+      visit_opening_from_home_page opening
+      application_submission = create(:application_submission, opening: opening)
+      applicant = application_submission.applicant
+      fill_applicant_details applicant
+      fill_submission_details application_submission
+      click_on 'Create Application submission'
+      expect_duplicate_submission_error
+    end
   end
 
   scenario 'Applying for opening from careers page' do
--- spec/support/features/application_submission_helpers.rb
+++ spec/support/features/application_submission_helpers.rb
@@ -11,5 +11,18 @@ module Features
       fill_in 'application_submission_cover_letter', with: submission.cover_letter
     end
 
+    def expect_submission_success
+      expect(current_path).to eq application_submission_verdict_path(verdict: 'success')
+      expect(page).to have_content "Congratulations !"
+      expect(page).to have_content "Your application was successfully submitted"
+    end
+
+    def expect_duplicate_submission_error
+      expect(current_path).to eq application_submission_verdict_path(verdict: 'failure')
+      expect(page).to have_content "We are Sorry!"
+      expect(page).to have_content "Your application could not be submitted"
+      expect(page).to have_content "You have already applied for this position"
+    end
+
   end
 end
