0c7b6c731dcab64bfd9c3cd77adca31c5ab6165e
Lorefnon <lorefnon@gmail.com>
2016-04-03 14:56:32 +0530

    Add custom selectors for active admin

--- spec/support/finders/active_admin.rb
+++ spec/support/finders/active_admin.rb
@@ -6,4 +6,28 @@ RSpec.configure do |config|
     end
   end
 
+  Capybara.add_selector(:menu_item) do
+    xpath do |label|
+      "//div[@id='header']/li/a[contains(text(), '#{label}')]"
+    end
+  end
+
+  Capybara.add_selector(:action_item) do
+    xpath do |label|
+      "//div[@class='action_item']/a[contains(text(), '#{label}')]"
+    end
+  end
+
+  Capybara.add_selector(:index_table) do
+    xpath do |model_name|
+      "//div[@class='index_as_table']/table[@id='index_table_#{model_name.to_s.underscore.pluralize}']"
+    end
+  end
+
+  Capybara.add_selector(:attributes_table) do
+    xpath do |model_name|
+      "//div[@class='attributes_table #{model_name.to_s.underscore.singularize}']"
+    end
+  end
+
 end
