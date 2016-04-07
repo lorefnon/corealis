57c831060076cb24238589f4832308b683a59387
Lorefnon <lorefnon@gmail.com>
2016-04-07 09:36:42 +0530

    Improve specs

---
+++ spec/admin/quiz_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'support/features/admin_session_helpers'
+require 'support/finders/active_admin'
+require 'support/matchers/active_admin'
+
+feature 'Quiz' do
+
+  include Features::AdminSessionHelpers
+
+  before(:each) do
+    @quiz = create(:quiz)
+    ensure_admin_signed_in
+    visit admin_quizzes_path
+  end
+
+  context "Index table" do
+    subject { page.find :index_table, :quiz }
+    %i[id title duration slug created_at updated_at].each do |col|
+      it { is_expected.to have_index_column col }
+    end
+  end
+
+end
--- spec/support/matchers/active_admin.rb
+++ spec/support/matchers/active_admin.rb
@@ -2,7 +2,7 @@ RSpec::Matchers.define :have_attribute_row do |label|
   match do |table|
     begin
       table
-        .find(:xpath, "/tr[contains(@class, 'row')]/th[contains(text(), '#{label}')]")
+        .find(:xpath, "tr[contains(@class, 'row')]/th[contains(text(), '#{label}')]")
         .present?
     rescue Capybara::ElementNotFound
       false
@@ -10,20 +10,14 @@ RSpec::Matchers.define :have_attribute_row do |label|
   end
 end
 
-RSpec::Matchers.define :have_index_column do |label|
+RSpec::Matchers.define :have_index_column do |field|
   match do |table|
     begin
       table
-        .find(:xpath, "/thead/tr/th[contains(@class, 'row')]/a[contains(text(), '#{label}')]")
+        .find(:xpath, "thead/tr/th[contains(@class, 'col')][contains(@class, 'col-#{field}')]")
         .present?
     rescue Capybara::ElementNotFound
       false
     end
-  end
-end
-
-RSpec::Matchers.define :have_index_columns do |labels|
-  match do |table|
-    labels.map{|col| has_index_column? label }.reduce(:&)
   end
 end
