a43e04d7d1f08a74f4505ce11050956f3fb3f0b3
Lorefnon <lorefnon@gmail.com>
2016-04-10 22:05:48 +0530

    Cleanup

--- app/assets/javascripts/active_admin.js.rb
+++
@@ -1,5 +0,0 @@
-#= require active_admin/base
-
-require 'opal'
-require 'opal_ujs'
-require 'opal-jquery'
--- app/assets/javascripts/application.js.rb
+++
@@ -1,4 +0,0 @@
-require 'opal'
-require 'opal_ujs'
-require 'opal-jquery'
-require_tree './common'
