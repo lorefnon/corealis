6af627c707d30d360057e9464a7cc074ede38d92
Lorefnon <lorefnon@gmail.com>
2016-04-15 16:53:33 +0530

    Add mailer specs

--- spec/mailers/invitation_mailer_spec.rb
+++ spec/mailers/invitation_mailer_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 RSpec.describe InvitationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @org_name = Setting['organization.name'] = 'Random Org'
+    @invitation = create(:invitation)
+  end
+
+  describe '#invitation_mail' do
+    subject { InvitationMailer.invitation_mail(@invitation).deliver_now }
+    it 'is delivered' do
+      expect(ActionMailer::Base.deliveries).to_not be_empty
+    end
+    it 'is dispatched to invitee' do
+      expect(subject.to).to include @invitation.invitee.email
+    end
+    it 'has correct subject' do
+      expect(subject.subject).to eql "Career opportunity at #{@org_name}"
+    end
+  end
+
 end
