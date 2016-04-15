b5804a429de196edadd60fd4642447ecff2cf28c
Lorefnon <lorefnon@gmail.com>
2016-04-15 14:01:13 +0530

    Cleanup existing specs

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -11,29 +11,33 @@ feature 'Home Page' do
     Setting['organization.description'] = @organization_description = 'Lorem ipsum dolor sit amet'
   end
 
-  scenario 'home page is visited' do
+  scenario 'User visits home page' do
     visit root_path
     expect(page).to have_content @organization_name
     expect(page).to have_content @organization_description
   end
 
-  scenario 'Openings are available' do
-    openings = create_openings_of_all_variants
-    visit root_path
-    openings.each do |(current_status, showcased_status), opening|
-      if current_status == :current && showcased_status == :showcased
-        expect(page).to have_link opening.title, href: opening_path(opening)
-      else
-        expect(page).to_not have_link opening.title, href: opening_path(opening)
+  context 'Openings are available' do
+    scenario 'User browses openings on home page' do
+      openings = create_openings_of_all_variants
+      visit root_path
+      openings.each do |(current_status, showcased_status), opening|
+        if current_status == :current && showcased_status == :showcased
+          expect(page).to have_link opening.title, href: opening_path(opening)
+        else
+          expect(page).to_not have_link opening.title, href: opening_path(opening)
+        end
       end
+      expect(page).to have_link 'View All', href: openings_path
     end
-    expect(page).to have_link 'View All', href: openings_path
   end
 
-  scenario 'No openings are available' do
-    Opening.destroy_all
-    visit root_path
-    expect(find('.zilch-container')).to have_content 'There are no open positions right now'
+  context 'No openings are available' do
+    scenario 'User browses openings on home page' do
+      Opening.destroy_all
+      visit root_path
+      expect(find('.zilch-container')).to have_content 'There are no open positions right now'
+    end
   end
 
 end
