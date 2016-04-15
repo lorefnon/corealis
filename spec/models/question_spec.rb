51151173a7bd06c4a843752359d36095fd017b55
Lorefnon <lorefnon@gmail.com>
2016-04-15 14:01:17 +0530

    Improve specs

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :creator }
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :creator_id }
 end
 
 # == Schema Information
