ffc77ed1204d95b178523682e4e29c4aa559b5ad
Lorefnon <lorefnon@gmail.com>
2016-04-15 22:58:13 +0530

    Add stub features for participation in quiz session

---
+++ spec/features/quiz_session_participation_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'Quiz Session participation' do
+
+  scenario 'User visits quiz session from invitation email' do
+
+  end
+
+  scenario 'User revisits quiz session from invitation email' do
+  end
+
+  scenario 'User answers questions' do
+  end
+
+  scenario 'User completes interview' do
+  end
+
+  scenario 'User revisits previous question' do
+  end
+
+end
